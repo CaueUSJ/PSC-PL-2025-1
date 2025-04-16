c929fbe4c4d23adf2fc0af96c1281118e3fed41a
Cauê Lopes Justo - 82329174 <82329174@ulife.com.br>
2025-04-16 10:43:40 -0300

    Teste main

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        Aluno caue = new Aluno("Cauê ", 24, 1.72, "82329174@ulife.com.br", "82329174", 0);
         
     }
-    
+
 }
